1b8fa9333a368cb068668476e67bd7781674dcdf
Prashant Singh <prashantsingh@Prashants-MacBook-Air.local>
2024-06-05 15:21:52 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const bodyParser = require('body-parser');
+const express=require('express');
+const app=express();
+const {PORT} =require('./config/serverConfigs');
+const apiRoutes=require('./routes/index');
+const db=require('./models/index');
+
+const setupAndStartServer=()=>{
+
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended:true}));
+    app.use('/api',apiRoutes);
+
+
+    app.listen(PORT,()=>{
+        console.log(`Server Started on port ${PORT}"`);
+
+        if(process.env.DB_SYNC){
+            db.sequelize.sync({alter:true});
+        }
+    });
+}
+setupAndStartServer();
